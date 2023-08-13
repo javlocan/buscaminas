b77f59ec3fa03891e4b83d1bc1924edca819e708
javlocan <javlocan.dev@gmail.com>
2023-08-13 11:17:49 +0200

    Desktop scrolling fix and few changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,8 @@ import App from "./App.tsx";
 
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
